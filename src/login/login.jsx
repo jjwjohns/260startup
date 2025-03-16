bb2c86decef3e2d02abcc9e5f8e4d96216722785
jjwjohns <jordanwjohns@gmail.com>
2025-03-16 15:49:41 -0600

    unauthenticated

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import './login.css';
-import { Link } from 'react-router-dom';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authstate';
+
+import './login.css';
+
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main id="pmain" className="container-fluid text-center">
-      <h1>Welcome to Online Mancala</h1>
-      <h3>Sign in to play!</h3>
-      <form method="get" action="play.html">
-        <div className="input-group mb-3">
-          <span className="input-group-text">@</span>
-          <input className="form-control" type="text" placeholder="username@email.com" />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text">🔒</span>
-          <input className="form-control" type="password" placeholder="password" />
-        </div>
-        <div>
-            <Link to="play" className="btn btn-primary">Login</Link>
-            <Link to="play" className="btn btn-secondary">Create</Link>
-        </div>
-      </form>
+      {authState !== authState.Unknown && <h1>Welcome to Online Mancala</h1>}
+
+      {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+      {authState === AuthState.Unauthenticated && (
+        <Unauthenticated
+          userName={userName}
+          onLogin={(loginUserName) => {
+            onAuthChange(loginUserName, AuthState.Authenticated);
+          }}
+        />
+      )}
     </main>
   );
 }
---
+++ src/login/unauthenticated.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export function Unauthenticated(props) {
+    const [userName, setUserName] = React.useState(props.userName);
+    const [password, setPassword] = React.useState('');
+    const [displayError, setDisplayError] = React.useState(null);
+
+    async function loginUser() {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+      }
+    
+      async function createUser() {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+      }
+
+      return (
+        <div>
+                <h3>Sign in to play!</h3>
+                <form method="get" action="play.html">
+                    <div className="input-group mb-3">
+                    <span className="input-group-text">@</span>
+                    <input className="form-control" type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username@email.com" />
+                    </div>
+                    <div className="input-group mb-3">
+                    <span className="input-group-text">🔒</span>
+                    <input className="form-control" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+                    </div>
+                    <div>
+                        <Link to="play" className="btn btn-primary">Login</Link>
+                        <Link to="play" className="btn btn-secondary">Create</Link>
+                    </div>
+                </form>
+
+                <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
+                    Login
+                </Button>
+                <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
+                    Create
+                </Button>
+
+                <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+        </div>
+        );
+}
