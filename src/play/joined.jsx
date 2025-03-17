27a8040c43319b32cc1e26e25f95970576f537f1
jjwjohns <jordanwjohns@gmail.com>
2025-03-16 23:34:08 -0600

    mancala board displaying array

--- src/play/joined.jsx
+++ src/play/joined.jsx
@@ -6,7 +6,7 @@ import './play.css';
 
 export function Joined(props) {
     const [mancalaSlots, setMancalaSlots] = React.useState(localStorage.getItem('mancalaSlots') || [0,4,4,4,4,4,4,0,4,4,4,4,4,4]);
-    
+
 
     React.useEffect(() => {
             const mancalaArray = localStorage.getItem('mancalaSlots');
@@ -14,6 +14,11 @@ export function Joined(props) {
               setMancalaSlots(JSON.parse(mancalaArray));
             }
         }, []);
+
+    React.useEffect(() => {
+          localStorage.setItem('mancalaSlots', JSON.stringify(mancalaSlots));
+        }, [mancalaSlots]);
+
 
     function onPressedQuit() {
         let index = parseInt(localStorage.getItem('currentGame'));
@@ -30,29 +35,32 @@ export function Joined(props) {
         localStorage.removeItem('currentGame');
     }
 
+    function onPressedPit(pitIndex) {
+    }
+
     return (
         <div id ="joined" className = "d-flex justify-content-center">
             <div className="mancala-board">
-                <div className="store left-store">0</div>
+                <div className="store left-store">{mancalaSlots[0]}</div>
                 <div className="pits-container">
                     <div className="row top-row">
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
+                        <div className="pit">{mancalaSlots[13]}</div>
+                        <div className="pit">{mancalaSlots[12]}</div>
+                        <div className="pit">{mancalaSlots[11]}</div>
+                        <div className="pit">{mancalaSlots[10]}</div>
+                        <div className="pit">{mancalaSlots[9]}</div>
+                        <div className="pit">{mancalaSlots[8]}</div>
                     </div>
                     <div className="row bottom-row">
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
-                        <div className="pit">4</div>
+                        <div className="pit">{mancalaSlots[1]}</div>
+                        <div className="pit">{mancalaSlots[2]}</div>
+                        <div className="pit">{mancalaSlots[3]}</div>
+                        <div className="pit">{mancalaSlots[4]}</div>
+                        <div className="pit">{mancalaSlots[5]}</div>
+                        <div className="pit">{mancalaSlots[6]}</div>
                     </div>
                 </div>
-                <div className="store right-store">0</div>
+                <div className="store right-store">{mancalaSlots[7]}</div>
             </div>
             <Button className="custom-button" variant='secondary' onClick={() => onPressedQuit()}>
                 Quit
