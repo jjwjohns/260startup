dd3d81b7be7ccef590ed95f3cfc8c417eeceaed3
jjwjohns <jordanwjohns@gmail.com>
2025-04-03 14:28:33 -0600

    added some more db functions

--- database/database.js
+++ database/database.js
@@ -5,7 +5,7 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('games');
+const gamesCollection = db.collection('games');
 
 (async function testConnection() {
   try {
@@ -15,3 +15,35 @@ const scoreCollection = db.collection('games');
     process.exit(1);
   }
 })();
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  
+  async function addUser(user) {
+    await userCollection.insertOne(user);
+  }
+  
+  async function updateUser(user) {
+    await userCollection.updateOne({ email: user.email }, { $set: user });
+  }
+
+  async function addGame(game) {
+    return gamesCollection.insertOne(game);
+  }
+
+  async function getGames() {
+    return gamesCollection.find({}).toArray();
+  }
+
+  async function getGame(id) {
+    return gamesCollection.findOne({ id: id });
+  }
+  
+  async function deleteGame(id) {
+    return gamesCollection.deleteOne({ id: id });
+  }
